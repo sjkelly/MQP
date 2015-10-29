5f2820cb7740f7c4b1efb95b32936218362d04fd
Steve Kelly <kd2cca@gmail.com>
2015-10-28 20:24:08 -0400

    I spent way too much time on this.

--- proposal/build.sh
+++ proposal/build.sh
@@ -1,3 +1,9 @@
+#! /bin/bash
+evince mqp-proposal-sjkelly.pdf &
+while true
+do
+inotifywait proposal.tex
 pdflatex -jobname=mqp-proposal-sjkelly proposal.tex
 bibtex mqp-proposal-sjkelly.aux
 pdflatex -jobname=mqp-proposal-sjkelly proposal.tex
+done
